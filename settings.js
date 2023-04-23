18ab0d3b892c5b7e747ee67a43875a9d65acf0cd
Fika12345 <130516720+Fika12345@users.noreply.github.com>
2023-04-23 11:09:55 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ edit sewabot di file settings.js line 24
 global.firtsChat = true //send firts message to new user
 global.autoread = true // auto read message
 global.antidelete = false // anti delete
-global.antiviewonce = true // anti view once
+global.antiviewonce = false // anti view once
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call, jika true maka org yg nelpon bot auto di blokir
 global.userRegister = false
@@ -46,19 +46,19 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "`Chaneedyou" //namabot kalian
+global.ownername = "chaaannn" //nama kalian
+global.myweb = "https://s.id/chaannn" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@chaaannn" //bebas asal jan hapus
+global.myweb2 = "https://s.id/zafika" //bebas
+global.email = "zafika977@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['6283182628110'] //premium user
+global.owner = ["6283182628110", "6282163403132"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283182628110" // nomor wa kalian
+global.ownernomerr = "+6282163403132"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
@@ -74,7 +74,7 @@ global.typemenu2 = 'image' //
 global.multiplier = 69
 global.limitawal = {
    premium: "Infinity", //unlimited
-   free: 100
+   free: 1000
 }
 global.buttonDefault = [{
    urlButton: {
